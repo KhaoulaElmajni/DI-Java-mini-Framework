c8034144aec88d2cff89b3949760ddd564207be9
khaoula elmajni <elmajnikhaoula99@gmail.com>
2022-04-02 12:30:43 +0000

    adding injection with setter and constructeur in xml file

--- IOC-Framework/src/main/java/me/khaoula/service/imp/PersonneServiceImp.java
+++ IOC-Framework/src/main/java/me/khaoula/service/imp/PersonneServiceImp.java
@@ -2,10 +2,21 @@ package me.khaoula.service.imp;
 
 
 import me.khaoula.framework.annotations.Component;
+import me.khaoula.service.JobService;
 import me.khaoula.service.PersonneService;
+
 
 @Component
 public class PersonneServiceImp implements PersonneService {
+
+    JobService jobService;
+
+    public PersonneServiceImp(Object o) {
+
+    }
+
+    public void setJobService(JobServiceImp jobService) {
+    }
     @Override
     public String getPersonneName() {
         return "PersonneName";
