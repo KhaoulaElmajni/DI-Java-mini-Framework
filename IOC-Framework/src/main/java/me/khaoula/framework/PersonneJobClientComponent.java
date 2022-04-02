b7877624bb028294b2d61308efff7713742f9adf
khaoula elmajni <elmajnikhaoula99@gmail.com>
2022-04-02 23:13:42 +0000

    modification in service

--- IOC-Framework/src/main/java/me/khaoula/framework/PersonneJobClientComponent.java
+++ IOC-Framework/src/main/java/me/khaoula/framework/PersonneJobClientComponent.java
@@ -6,13 +6,10 @@ import me.khaoula.framework.annotations.Component;
 import me.khaoula.framework.annotations.Qualifier;
 import me.khaoula.service.JobService;
 import me.khaoula.service.PersonneService;
-
 @Component
 public class PersonneJobClientComponent {
-
     @Autowired
     private PersonneService personneService;
-
     @Autowired
     @Qualifier(value = "jobServiceImp")
     private JobService jobService;
--- IOC-Framework/src/main/java/me/khaoula/service/imp/PersonneServiceImp.java
+++ IOC-Framework/src/main/java/me/khaoula/service/imp/PersonneServiceImp.java
@@ -1,6 +1,7 @@
 package me.khaoula.service.imp;
 
 
+import me.khaoula.framework.annotations.Autowired;
 import me.khaoula.framework.annotations.Component;
 import me.khaoula.service.JobService;
 import me.khaoula.service.PersonneService;
@@ -8,13 +9,10 @@ import me.khaoula.service.PersonneService;
 
 @Component
 public class PersonneServiceImp implements PersonneService {
-
+    @Autowired
     JobService jobService;
-
     public PersonneServiceImp(Object o) {
-
     }
-
     public void setJobService(JobServiceImp jobService) {
     }
     @Override
